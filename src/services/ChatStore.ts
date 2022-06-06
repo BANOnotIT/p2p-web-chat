7160d5fdb8ce9638abdd14689082e481c510f5b3
BANO.notIT <bano.notit@yandex.ru>
2022-06-06 18:58:20 +0300

    feat: Add synchronization in ChatStore

--- src/services/ChatStore.ts
+++ src/services/ChatStore.ts
@@ -3,25 +3,59 @@ import { AESCBCCryptor } from "./AESCBCCryptor";
 import { ChatBuf } from "../protobuf/Chat.buf";
 import { UserMessageBuf } from "../protobuf/UserMessage.buf";
 import hypercore, { HyperCoreFeed } from "hypercore";
+import { DiscoveryManager } from "./DiscoveryManager";
+import { HypercoreSynchronize } from "./HypercoreSynchronize";
+import Peer from "simple-peer";
 
 export class ChatStore {
   private cryptor: AESCBCCryptor;
   private myMessagesCore: HyperCoreFeed<Buffer>;
   private theirMessagesCore: HyperCoreFeed<Buffer>;
 
+  private myMessagesSynchronize: HypercoreSynchronize;
+  private theirMessagesSynchronize: HypercoreSynchronize;
+
+  private chatInfoHash: null | string = null;
+
   constructor(
     private chatId: NonNullable<ChatEntity["id"]>,
-    private insides: ChatBuf
+    private insides: ChatBuf,
+    private discoveryMgr: DiscoveryManager,
   ) {
     this.cryptor = AESCBCCryptor.fromU8Array(insides.sharedSecret);
 
     const myStore = messagesRandomAccessDB(`chat-messages-${insides.uuid}:my`);
     const theirStore = messagesRandomAccessDB(
-      `chat-messages-${insides.uuid}:their`
+      `chat-messages-${insides.uuid}:their`,
     );
 
     this.myMessagesCore = hypercore(myStore);
     this.theirMessagesCore = hypercore(theirStore);
+
+    this.myMessagesSynchronize = new HypercoreSynchronize(
+      this.myMessagesCore,
+      "chat-messages",
+      true,
+      this.discoveryMgr.selfId,
+    );
+    this.theirMessagesSynchronize = new HypercoreSynchronize(
+      this.theirMessagesCore,
+      "chat-messages",
+      false,
+      this.discoveryMgr.selfId,
+    );
+
+    this.discoveryMgr.on("peer connected", this.peerConnectedListener);
+  }
+
+  destroy() {
+    this.discoveryMgr.off("peer connected", this.peerConnectedListener);
+  }
+
+  async connectToPeers() {
+    this.chatInfoHash = await this.discoveryMgr.createAnnounce(
+      this.insides.sharedSecret.toString(),
+    );
   }
 
   async getMessages(): Promise<UserMessageBuf[]> {
@@ -37,7 +71,7 @@ export class ChatStore {
         (err: null | Error) => {
           if (err) return error(err);
           done(encryptedBlob);
-        }
+        },
       );
     });
   }
@@ -50,4 +84,19 @@ export class ChatStore {
     const encoded = UserMessageBuf.encode(entity).finish();
     return this.cryptor.encrypt(encoded);
   }
+
+  private peerConnectedListener = ({
+    peer,
+    infoHash,
+    peerId,
+  }: {
+    peer: Peer.Instance;
+    infoHash: string;
+    peerId: string;
+  }) => {
+    if (infoHash === this.chatInfoHash) {
+      this.myMessagesSynchronize.registerRTCPeer(peer, peerId);
+      this.theirMessagesSynchronize.registerRTCPeer(peer, peerId);
+    }
+  };
 }
